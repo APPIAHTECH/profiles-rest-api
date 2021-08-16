22c6d474490871ace108c8d8400adffba3283198
stephen <stephen@trialing.org>
2021-08-16 13:25:05 +0200

    add http methods

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,24 +1,43 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
 
 # Create your views here.
-class HelloApiView( APIView ):
+class HelloApiView(APIView):
+    """Test API View"""
+    serializer_class = serializers.HelloSerializer
 
-    def get(self , req, format=None):
-        an_apiview = [
-            'Uses http methods as fun (get, post, patch, put, delete)',
-            'Is similiar to a traditional django view',
-            'Is mapped manually yo URLs'
-        ]
-
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
         return Response({
-            'message': 'Hey',
-            'an_apiview': an_apiview
+            "message": "hola"
         })
 
-    def post(self , req, format=None): pass
-    def put(self , req, format=None): pass
-    def patch(self , req, format=None): pass
-    def delete(self , req, format=None): pass
-    
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, pk=None):
+        """ Handle updating all object value"""
+        return Response({'method': "Put"})
+
+
+    def patch(self, request, pk=None):
+        """ Handle updating field in object"""
+        return Response({'method': "patch"})
+
+    def delete(self, request, pk=None):
+        """ Handle updating field in object"""
+        return Response({'method': "delete"})
